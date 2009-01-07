351061b2971a02ce849afe201dece5f85c976eb2
surat pyari <surat@satish.(none)>
2009-01-07 12:33:54 +0530

    show page for forum_post added

---
+++ app/controllers/forum_posts_controller.rb
@@ -0,0 +1,16 @@
+class Member::ForumPostsController < ApplicationController
+  
+  before_filter :find_forum_post, :only => :show
+  
+  private #########################
+  
+  def find_forum_post
+    @forum_post = ForumPost.find(params[:id]) rescue nil
+    unless @forum_post
+      flash[:error] = "Sorry, the page cannot be found."
+      redirect_to member_forums_path
+    end
+  end
+ 
+end
+ 
